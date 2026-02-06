cd7bc6a73c6e8a8316b44df8bac3a59d93000660
Amelia Karamanos <ank5688@psu.edu>
2026-02-06 12:20:34 -0500

    5 card instances with easy to change variables for homework assignment.

--- src/my-card.js
+++ src/my-card.js
@@ -14,24 +14,74 @@ export class MyCard extends LitElement {
 
   constructor() {
     super();
-    this.title = "My card";
+    this.title = "Clippy Card!";
+    this.link = "https://hax.psu.edu";
+    this.image="https://i.pinimg.com/originals/21/86/a6/2186a6ede0392eae08c4297a3ac3b84a.gif";
+    this.alt="GIF of Clippy reading a document";
+    this.btn="Details";
+
   }
 
   static get styles() {
     return css`
-      :host {
-        display: block;
-      }
+        .card {
+  text-align: center;
+  background-color: var(--my-card-bg-color, #D0D7E7);
+  max-width: 400px;
+  margin: 16px auto;
+  padding: 16px;
+}
+
+.card-image {
+  width: 300px;
+  border: 10px solid #12152B;
+  border-radius: 10px;
+}
+
+h1 {
+  color: #12152B;
+}
+
+.btn {
+  color: #12152B;
+  font-family: "Times New Roman", Times, serif;
+  font-size: 32px;
+  margin: 10px 50px 100px;
+  padding: 20px 80px;
+  border: 10px solid #12152B;
+  border-radius: 10px;
+}  
+  .btn {
+    font-size: 20px;
+    padding: 12px 40px;
+    border-width: 6x;
+  }
+
     `;
   }
 
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`
+<div id="cardlist" class="wrapper">
+  <div class="card">
+  <h2 class="card-title">${this.title}</h2>
+  <img class="card-image" src="${this.image}" alt="${this.alt}"> 
+
+  <a href="${this.link}" class="details-btn">
+    <button class="btn"> ${this.btn}  </button>
+  </a>
+  </div>
+</div>
+`;
   }
 
   static get properties() {
     return {
       title: { type: String },
+      link: { type: String },
+      image: { type: String },
+      alt: { type: String },
+      btn: { type: String }
     };
   }
 }
